b4f1f219e56a48111355347fa1d22e1dbb836f5d
Fikrie <Muhammad.Fikrie@bni.co.id>
2023-01-28 08:32:40 +0700

    Update Functionality onClick and CLR

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun onDigit(view: View){
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
-        }
+        tvInput = findViewById(R.id.tvInput)
     }
 
-    fun onDigit(view: View) {}
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
 }
