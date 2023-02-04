aeaab261f28f57a44e1ec05c6f91b5e65c2b4a69
Fikrie <Muhammad.Fikrie@bni.co.id>
2023-02-04 11:00:34 +0700

    Adding Subtraction and the missing Operations

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var tvInput : TextView? = null
     var lastNumeric : Boolean = false
     var lastDot : Boolean = false
+    var firstMinus : Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity() {
 
     fun onClear(view: View){
         tvInput?.text = ""
+        lastNumeric = false
+        lastDot = false
+        firstMinus = false
     }
 
     fun onDecimalPoint(view: View){
@@ -41,12 +45,85 @@ class MainActivity : AppCompatActivity() {
 
     fun onOperator(view: View){
         tvInput?.text?.let {
+            val opMinus : Boolean = (view as Button).text == "-"
             if(lastNumeric && !isOperationAdded(it.toString())){
                 tvInput?.append((view as Button).text)
                 lastNumeric = false
                 lastDot = false
+            }else if(opMinus && !lastNumeric && !firstMinus) {
+                if(!isOperationAdded(it.toString())){
+                    tvInput?.append((view as Button).text)
+                    lastNumeric = false
+                    lastDot = false
+                    firstMinus = true
+                }
             }
         }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result : String) : String{
+        var value = result
+        if(result.contains(".0")){
+            value = result.substring(0, result.length - 2)
+        }
+        return value
     }
 
     private fun isOperationAdded(value : String) : Boolean{
